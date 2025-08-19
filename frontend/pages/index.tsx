f9e215e41a6583acb92a4d0bfb2a4773d9eed8f5
Jotfmab <jotmc2fmab@gmail.com>
2025-08-20 00:00:03 +0530

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -328,7 +328,6 @@ export default function Home() {
           onCellValueChanged={onCellValueChanged}
           stopEditingWhenCellsLoseFocus
         />
-        print"HI"
       </div>
     </div>
   );
